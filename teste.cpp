68334bfdbd2409f2bf1ab3c79ccaf7548076beeb
jose-gomes1 <JG080420@edu.azores.gov.pt>
2023-10-06 10:32:58 +0000

    O

---
+++ teste.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+int NUM_PORT_SENS_HUM = 5;
+
+   int oMeuNumero = 10;   //números inteiros
+   double oMeuNumeroInteiro = 10.5;  //números reais
+   char oMeuCaracter = 'a';   //caracteres
+   string oMeuTexto = "Byte é a unidade de medida na informática";  //texto
+   bool oMeuBooleano = true;  //booleano  //1 = Verdadeiro; 0 = Falso
+   int myBool = 1;   //true
+   int sensorTemperatura = 10;
+   const double PI = 3.14159265359;   //constante
+   int x = 2, z = 3, y = 14;
+   int minutosPorHora=60;
+   int numero;  
+
+   int main()
+   {
+    cout << "Digite um número: ";   //imprime no ecrã
+    cin >> numero;  //lê do teclado
+    cout << "O número que digitaste foi:" << numero << endl;
+    return 0;
+   }
+
+
+   int main2()
+   {
+    cout << PI << " Valor de PI\n";   //comentário
+    cout << sensorTemperatura << " valor do sensor de temperatura\t";
+
+    sensorTemperatura = 20;
+    cout << sensorTemperatura << " valor do sensor de temperaura\t" << PI << "valor de PI\t";
+    return 0;
+   }
+/*
+*@author sou eu
+*teste
+*
+*/
+
+; int main3()
+{
+    cout << "Hello \\ World!\t";  //comentário
+    cout << "I'm a C++ program\n";   int minutosPorHora=60;
+
+    cout << oMeuTexto << endl; 
+    cout << "\n";
+    return 0;
+}
+
+    /**
+     * @brief
+     * 
+     * @param a número inteiro
+    */
+//1 byte = 8 bits
+//byte é a unidade de medida na informática
+//tipo da variável nomeDaVariavel = valor
+//nome da variável tem de ser em CamelCase e tem de ser intuitívo
+//escrever em inglês, não em português
