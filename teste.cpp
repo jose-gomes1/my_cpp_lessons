e43e7c7c6f81cb233aeb68192bd79162f1e803c2
jose-gomes1 <JG080420@edu.azores.gov.pt>
2023-10-18 11:18:20 +0000

    T T ^T bash: linha 1: M-: comando não encontrado

--- teste.cpp
+++ teste.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+
+
 int NUM_PORT_SENS_HUM = 5;
 
    int oMeuNumero = 10;   //números inteiros
@@ -13,9 +15,41 @@ int NUM_PORT_SENS_HUM = 5;
    const double PI = 3.14159265359;   //constante
    int x = 2, z = 3, y = 14;
    int minutosPorHora=60;
-   int numero;  
+   int numero; 
+   int raio; 
 
    int main()
+   {
+    int a = 5;
+    int b = 5;
+    int c = 2;
+    int d = 1;
+    int result = a&=4;
+    cout << result << endl;
+    return 0;
+   }
+
+   int main2()
+   {    
+    //tendo em consideração que a área do círculo é PI * raio * raio, calcula a área do círculo com raio digitado pelo usuário
+    cout << "Qual é o raio do círculo?" << endl;   //imprime no ecrã
+    cin >> raio;  //lê do teclado
+    double area = PI * raio * raio;
+    cout << area << endl;
+    return 0;
+   }
+
+   int main3()
+   {
+    //tendo em consideração que a área do círculo é PI * raio * raio, calcula a área do círculo com raio 10
+    int r = 10;
+    double area = PI * r * r ;
+    cout << area << endl;
+
+    return 0;
+   }
+
+   int main4()
    {
     cout << "Digite um número: ";   //imprime no ecrã
     cin >> numero;  //lê do teclado
@@ -24,7 +58,7 @@ int NUM_PORT_SENS_HUM = 5;
    }
 
 
-   int main2()
+   int main5()
    {
     cout << PI << " Valor de PI\n";   //comentário
     cout << sensorTemperatura << " valor do sensor de temperatura\t";
@@ -39,23 +73,13 @@ int NUM_PORT_SENS_HUM = 5;
 *
 */
 
-; int main3()
+; int main6()
 {
     cout << "Hello \\ World!\t";  //comentário
-    cout << "I'm a C++ program\n";   int minutosPorHora=60;
+    cout << "I'm a C++ program\n";   
+    int minutosPorHora=60;
 
     cout << oMeuTexto << endl; 
     cout << "\n";
     return 0;
 }
-
-    /**
-     * @brief
-     * 
-     * @param a número inteiro
-    */
-//1 byte = 8 bits
-//byte é a unidade de medida na informática
-//tipo da variável nomeDaVariavel = valor
-//nome da variável tem de ser em CamelCase e tem de ser intuitívo
-//escrever em inglês, não em português
