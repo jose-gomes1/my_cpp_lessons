c7dd8b7115fa4b80ad9b441e73f22bde794d08af
jose-gomes1 <JG080420@edu.azores.gov.pt>
2023-10-18 12:35:15 +0000

    conditions.cpp

---
+++ conditions.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int a=10, b=12, c=9;
+
+int main(){
+
+    if ( a < b){
+        cout << "CÊ TA CERTO" << endl;
+    }else if (a<c){
+        cout << "CÊ TA CERTO" << endl;
+    }else{
+        cout << "CÊ TA ERRADO" << endl;
+    }
+    
+
+
+
+    return 0;
+}
