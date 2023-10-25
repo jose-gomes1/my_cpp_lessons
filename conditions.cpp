570e2b6174a797d4235ddafaa157378535e7812f
jose-gomes1 <JG080420@edu.azores.gov.pt>
2023-10-25 12:07:41 +0000

    conditions.cpp

--- conditions.cpp
+++ conditions.cpp
@@ -59,7 +59,16 @@ default:
         {
             cout << "Qual o número da sorte?" << endl;
             cin >> num;
+            if(numSorte > num){
+                cout << "O número da sorte é maior que esse número, cabeça de meia tigela" << endl;
+            }else if(numSorte < num){
+                cout << "O número da sorte é menor que esse número, cabeça de meia tigela" << endl;
+            }else{
+                cout << "Parabéns! Acertaste o número, seu sem vida" << endl;
+            }
+            
         }
-        cout << "Acertaste" << endl;
-        
+        cout << "Acabou o jogo" << endl;
+    
+        return 0;   
     }
