6b311db6aaa1ce39a0ec2e2e47e8d53a0e5bcf16
jose-gomes1 <JG080420@edu.azores.gov.pt>
2023-10-27 09:16:26 +0000

    conditions.cpp

--- conditions.cpp
+++ conditions.cpp
@@ -59,16 +59,19 @@ default:
         int num=0;
         srand((unsigned) time (0));
         int numSorte= 1+(rand()%100);
+        int tries=0;
+        tries++;
         while (numSorte != num)
         {
             cout << "Qual o número da sorte?" << endl;
             cin >> num;
+            tries++;
             if(numSorte > num){
                 cout << "O número da sorte é maior que " << num << ", seu cabeça de meia tigela" << endl;
             }else if(numSorte < num){
                 cout << "O número da sorte é menor que " << num << ", seu cabeça de meia tigela" << endl;
             }else{
-                cout << "Parabéns! Acertaste o número, seu sem vida" << endl;
+                cout << "Parabéns! Acertaste o número, em " << tries - 1 << " tentativas, seu sem vida" << endl;
             }
             
         }
