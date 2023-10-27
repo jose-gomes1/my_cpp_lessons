93b57cfd6d1dba18ec9452bbe98466e464b5ad3e
jose-gomes1 <JG080420@edu.azores.gov.pt>
2023-10-27 09:18:42 +0000

    condition.cpp

--- conditions.cpp
+++ conditions.cpp
@@ -4,58 +4,7 @@
 
 using namespace std;
 
-int a=10, b=12, c=9;
-
-int main2(){
-
-    if ( a < b){
-        cout << "CÊ TA CERTO" << endl;
-    }else if (a<c){
-        cout << "CÊ TA CERTO" << endl;
-    }else{
-        cout << "CÊ TA ERRADO" << endl;
-    }
-    
-
-
-
-    return 0;
-}
-
-int main3(){
-    int num;
-    int result;
-    cout << "Digite um número." << endl;
-    cin >> num;
-    result = num % 2;
-    
-    if(num >= 0){
-        cout << "O número que escolheste é positivo." << endl;
-    }else if (num < 0){
-        cout << "O número que escolheste é negativo." << endl;
-    }
-
-    switch (result)
-{
-case 0: 
-    cout << "O número é par." << endl;
-    break;
-
-default:
-    cout << "O número não é par." << endl;
-    break;
-}
-
-    while (num < 5)
-    {
-        cout << "Digite um número." << endl;
-        cin >> num;
-    }
-
-    return 0;
-    }
-
-    int main(){
+ int main(){
         int num=0;
         srand((unsigned) time (0));
         int numSorte= 1+(rand()%100);
