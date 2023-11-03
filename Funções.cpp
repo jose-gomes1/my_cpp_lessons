229db32c633a70b4b1b2e8c2f488523a63ea175e
jose-gomes1 <145759337+jose-gomes1@users.noreply.github.com>
2023-11-03 10:27:41 -0100

    Funções.cpp

--- Funções.cpp
+++ Funções.cpp
@@ -14,7 +14,7 @@ int main(){
     
     for(int i = 0; i < 2; i++){
         for (int j = 0; j < 4; j++){
-        cout << letras2[i][0] << endl;
+        cout << letras2[i][j] << endl;
         }
     }
 
