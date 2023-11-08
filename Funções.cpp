bef78783bb83725296f82a479176bfbe1ad83df8
jose-gomes1 <145759337+jose-gomes1@users.noreply.github.com>
2023-11-08 12:15:24 -0100

    Funções.cpp

--- Funções.cpp
+++ Funções.cpp
@@ -10,18 +10,80 @@ string letras2[2][4] = {
     {"E", "F", "G", "H"}
 };
 
-int main(){
-    
-    for(int i = 0; i < 2; i++){
-        for (int j = 0; j < 4; j++){
-        cout << letras2[i][j] << endl;
-        }
-    }
+struct pessoa
+{
+    string nome;
+    int idade;
+    string CodPostal;
+    string rua;
+    string pais;
+    string cidade;
+};
 
-    //cout << letras2[1][2] << endl;
-    //cout << myArray[4] << endl;
-    //cout << sizeof(myArray)/4 << endl;
-    return 0;
+int f(int x){
+    return x*x;
+};
+
+void ola(){
+    cout << "Olá!" << endl;
+};
+
+void msgError(string msd){
+    cout << "--------------------" << endl;
+    cout << "Error: " << msd << endl;
+    cout << "--------------------" << endl;
 }
+
+
+void texto(string txt1, string txt2, string txt3){
+    cout << txt1 << endl;
+    cout << txt2 << endl;
+    cout << txt3 << endl;
+};
+
+int multiplicacao(int x, int y=2){
+    return x*y;
+};
+
+int main(){
+    // cout << f(5) << endl;
+    // cout << f(10) << endl;
+    // ola();
+
+    // msgError("Erro de conexão");
+
+    // msgError("Erro de leitura");
+    // texto("Olár", "Muindo", "!");
+    // cout << multiplicacao(x=2, b=4) << endl;
+
+    //uma função em C++ é uma instrução identificada com um determinado nome que pode ou não retornar um valor e pode ou não aceitar parâmetros dentro dos parênteses
+};
+
+// int main(){
+
+//     pessoa p1, p2;
+
+//     p1.nome = "João";
+//     p2.nome = "Maria";
+
+//     cout << p1.nome << endl;
+//     cout << p2.nome << endl;
+
+// }
+
+
+//   int main1(){
+    
+//    for(int i = 0; i < 2; i++){
+//        for (int j = 0; j < 4; j++){
+//        cout << letras2[i][j] << endl;
+//        }
+//    }
+
+//     cout << letras2[1][2] << endl;
+//     cout << myArray[4] << endl;
+//     cout << sizeof(myArray)/4 << endl;
+//    return 0;
+// }codeLLD
 
 
