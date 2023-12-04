3cac5697d7bbf5b3963f3c0ee35b29e01492fbc5
jose-gomes1 <145759337+jose-gomes1@users.noreply.github.com>
2023-12-04 10:26:32 -0100

    har har har har har

---
+++ har.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "har har har har\n";
+    cout << "har har har har\n";
+    cout << "har har har har\n";
+    cout << "har har har har\n";
+    cout << "har har har har\n";
+    cout << "har har har har\n";
+    cout << "har har har har\n";
+    cout << "har har har har\n";
+    cout << "har har har har\n";
+    cout << "har har har har\n";
+    cout << "har har har har\n";
+    cout << "har har har har\n";
+    cout << "har har har har\n";
+    cout << "har har har har\n";
+    cout << "har har har har\n";
+    cout << "har har har har\n";
+    cout << "har har har har\n";
+    cout << "har har har har\n";
+    cout << "har har har har\n";
+    cout << "har har har har\n";
+    cout << "har har har har\n";
+    cout << "har har har har\n";
+    cout << "har har har har\n";
+    cout << "har har har har\n";
+    cout << "har har har har\n";
+    cout << "har har har har\n";
+    return 0;
+}
