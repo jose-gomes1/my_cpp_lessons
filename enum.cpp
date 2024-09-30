29e0629bf66d46ca49a1062b0f64e6f9dc116af9
jose-gomes1 <145759337+jose-gomes1@users.noreply.github.com>
2024-09-30 16:42:01 +0000

    aaaaaaa

---
+++ enum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+enum Nivel{
+    Baixo,
+    Medio,
+    Alto
+}; //só pode tomar os valores definidos
+
+int main(){
+    Nivel xpto = Medio;
+    return 0;
+}
