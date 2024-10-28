8ee0c382f96edf1aef9281ab084b18602ffb5b27
José Gomes <JG080420@edu.azores.gov.pt>
2024-10-28 17:07:59 -0100

    fixed

--- aldeia-main.cpp
+++ aldeia-main.cpp
@@ -5,7 +5,7 @@ int main(){
     Wolves L1, L2;
     Villager V1, V2, V3, V4, V5, V6, V7, V8;
     //array de objetos
-    Vident vidente[V];
-    Wolves wolves[L1, L2];
-    Villager villagers[V1,V2,V3,V4,V5,V6,V7,V8];
+    Vident vidente[1] = {V};       // array of 1 Vident object
+    Wolves wolves[2] = { L1, L2 }; // array of 2 Wolves objects
+    Villager villagers[8] = { V1, V2, V3, V4, V5, V6, V7, V8 }; // array of 8 Villager objects
 }
