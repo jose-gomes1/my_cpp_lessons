c2cdf751c26f03c4a5ae32940a7782a187c28821
José Gomes <JG080420@edu.azores.gov.pt>
2024-11-11 10:22:20 -0100

    a

---
+++ date.cpp
@@ -0,0 +1,27 @@
+/**
+ * @file date.cpp
+ * @brief Implementação da classe Date
+ * @version 0.1
+ * 
+ * @author José Gomes
+ * @date 11-11-2024
+ */
+
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+/**
+ * @brief Funcão para limpar o ecrã
+ * @details Esta funcão usa as funções clear ou cls, dependendo do sistema operativo, para limpar o terminal
+ * @name clearScreen
+ * @param int valor desnecessário
+ * @return void
+ */
+void clearScreen(){
+    system("clear || cls");
+}
+
+int main(){
+
+}
