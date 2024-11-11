223d7c06dd729119dca4f24ce9237b95a4fa3e34
José Gomes <JG080420@edu.azores.gov.pt>
2024-11-11 09:48:00 -0100

    a

--- unexistingFile.cpp
+++ unexistingFile.cpp
@@ -6,19 +6,27 @@ using namespace std;
 //Abrir ficheiro que não existe
 
 int main(){
+
+while(true){
+    try{
     string nome;
     cout << "Qual é o nome do ficheiro?\n";
     cin >> nome;
-
-    try{
         ifstream file(nome);
         if(!file.is_open()){
-            throw "O ficheiro não existe!";
+            throw "E";
         }
-        throw "O ficheiro foi aberto com sucesso";
+        throw "O";
         }
-        catch(const char* e){
-            cout << e << endl;
-    }
+        catch(const char *e){
+            if(e[0] == 'E'){
+                cout << "Erro ao abrir o ficheiro" << endl;
+            }else{
+                cout << "Ficheiro aberto com sucesso" << endl;
+                break;
+            }
+}
+}
+    cout << "Fim do programa\n";
     return 0;
 }
