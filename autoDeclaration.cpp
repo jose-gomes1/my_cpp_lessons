8911164e45bf65d4e58d44e22adee1d10d83f46f
José Gomes <JG080420@edu.azores.gov.pt>
2024-12-05 11:37:18 -0100

    autoDeclaration.cpp

---
+++ autoDeclaration.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+string name = "Paulo";
+int number = 123;
+float numberfloat = 3.14;
+auto naoSei = "Não sei"; //apenas para variáveis sem certeza
+unsigned int altura = 10; //não pode ser negativo
+
+int main(){
+
+    cout << "Escape\\Characters\n";
+
+}
