a1a8da234e23baf496f96c0c9d3a181eb53dccb7
José Gomes <JG080420@edu.azores.gov.pt>
2024-12-05 11:54:20 -0100

    autoDeclaration.cpp

--- autoDeclaration.cpp
+++ autoDeclaration.cpp
@@ -8,8 +8,28 @@ float numberfloat = 3.14;
 auto naoSei = "Não sei"; //apenas para variáveis sem certeza
 unsigned int altura = 10; //não pode ser negativo
 
-int main(){
+void testes(){
+    int a;
+    a = 'A'; //não dá com "", pois assume como string
 
+    cout << a << endl; //valor ASCII de 'A'
     cout << "Escape\\Characters\n";
 
+    char lower = 'd';
+    char upper = lower - 32;
+    cout << upper << endl;
+
+}
+
+void maiusculas(){
+    char letraMin;
+    cout << "Insira uma letra minúscula: ";
+    cin >> letraMin;
+    cin.ignore();
+    cout << "A letra " << letraMin << " em maiúsculas é: " << (char)(letraMin - 32) << endl;
+}
+
+int main(){
+    maiusculas();
+    return 0;
 }
